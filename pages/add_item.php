aa9b4ebcf525fcb804341425b0996103c53ff84c
Artem Salikhov <artemsalikhov@mail.ru>
2021-12-23 19:38:18 +0700

    add css add-form

--- pages/add_item.php
+++ pages/add_item.php
@@ -1,28 +1,23 @@
 <?php
 ?>
-<div>
+<link rel="stylesheet" href="../css/index.css">
+<body>
     <?php require '../components/header.php'?>
     <section class="add-item">
         <div class="add-item__title">Save / Editor form</div>
         <div>
-            <form action="/" method="POST">
+            <form class="add-item__form" action="/" method="POST">
                 <input type="text" name="id" value="<?= $_GET['id'] ?? null ?>" style="display: none">
-                <label>
-                    Name
-                    <input type="text" name="name" placeholder="name" value="<?= $_GET['name'] ?? '' ?>">
-                </label>
-                <label>
-                    Phone
-                    <input type="number" name="phone" placeholder="phone" value="<?= $_GET['phone'] ?? '' ?>">
-                </label>
-                <label>
-                    Description
-                    <textarea name="text" placeholder="description"><?= $_GET['text'] ?? '' ?></textarea>
-                </label>
-                <input class="btn btn-edit" type="submit">
+                <span>Name</span>
+                <input type="text" name="name" placeholder="name" value="<?= $_GET['name'] ?? '' ?>">
+                <span>Phone</span>
+                <input type="number" name="phone" placeholder="phone" value="<?= $_GET['phone'] ?? '' ?>">
+                <span>Description</span>
+                <textarea class="add-item__textarea" name="text" placeholder="description"><?= $_GET['text'] ?? '' ?></textarea>
+                <input class="btn btn-edit" type="submit" value="send">
             </form>
         </div>
     </section>
   <?php require '../components/footer.php'?>
-</div>
+</body>
 
